f1398eced153ab71630cffdc7ff1f02559702360
inma06 <pakbongho@gmail.com>
2021-05-20 00:10:40 +0900

    - 삭제   - 컴포넌트 커스텀 앱바

--- lib/src/pages/calendar.dart
+++ lib/src/pages/calendar.dart
@@ -1,4 +1,3 @@
-import 'package:fearless_assemble/src/components/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';

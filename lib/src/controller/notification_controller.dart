2d5dac1488508d8dbb2cac9a963c798d8da8b2f7
inma06 <pakbongho@gmail.com>
2021-05-26 07:28:22 +0900

    셋팅, 노티 페이지 머지완료

--- lib/src/controller/notification_controller.dart
+++ lib/src/controller/notification_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
@@ -7,15 +9,88 @@ class NotificationController extends GetxController {
   OneSignal _oneSignal = OneSignal();
   String oneSignalID = "6395ae43-f8e5-4862-b70a-da8402d413c8";
   FirebaseMessaging _messaging = FirebaseMessaging();
+  // FlutterLocalNotificationsPlugin _flutterLocalNoti = FlutterLocalNotificationsPlugin();
   RxMap<String, dynamic> message = Map<String, dynamic>().obs;
-
   @override
   void onInit() {
     _initOneSignal();
     _initNotification();
     _getToken();
+    // _initLocalNoti();
+    _receivedHandler();
+    _openedHandler();
     super.onInit();
   }
+
+  /* LOCAL NOTIFICATION START */
+  // _initLocalNoti() {
+  //   AndroidInitializationSettings androidInitSet =
+  //       AndroidInitializationSettings('@mipmap/ic_launcher');
+  //   IOSInitializationSettings iosInitSet = IOSInitializationSettings();
+  //   InitializationSettings initSet =
+  //       InitializationSettings(android: androidInitSet, iOS: iosInitSet);
+  //
+  //   _flutterLocalNoti.initialize(initSet,
+  //       onSelectNotification: _onSelectLocalNoti);
+  // }
+  //
+  // _printPayload(String payload) {
+  //   print("_printPayload : $payload");
+  // }
+  //
+  // Future<void> _onSelectLocalNoti(String payload) async {
+  //   await _printPayload(payload);
+  // }
+
+  /* LOCAL NOTIFICATION END */
+
+  // 노티 수신시
+  _receivedHandler() {
+    _oneSignal.setNotificationReceivedHandler((OSNotification notification) {
+      // a notification has been received
+      print(
+          "_received androidNotificationId : ${notification.androidNotificationId}");
+      print("_received appInFocus : ${notification.appInFocus}");
+      print(
+          "_received jsonRepresentation : ${notification.jsonRepresentation()}");
+    });
+  }
+
+  // 노티 클릭시
+  _openedHandler() {
+    _oneSignal
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      // a notification has been opened
+      //
+      // NotiData notiData = NotiData.fromJson(
+      //     jsonDecode(result.notification.jsonRepresentation()));
+      // print("NotiData : ${notiData}");
+
+      print("_openedHandler_ result : $result.");
+    });
+  }
+
+  _initSetLogLevel() {
+    _oneSignal.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  }
+
+  /* OneSignal Setting Start */
+  // OneSignal 초기화
+  Future<void> _initOneSignal() async {
+    _oneSignal.init(oneSignalID, iOSSettings: {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.inAppLaunchUrl: false,
+    });
+    /*
+    * none -> Foreground 일때 노티X (데이터는 수신)
+    * notification -> Foreground & Background & Terminated 모두 노티
+    * alert -> Foreground 일때 AlertDialog ( 커스텀x )
+    * */
+    _oneSignal.setInFocusDisplayType(OSNotificationDisplayType.notification);
+    await _oneSignal.promptUserForPushNotificationPermission(
+        fallbackToSettings: true);
+  }
+/* OneSignal SETTINGS END */
 
   /* Google FCM Setting Start */
   Future<void> _getToken() async {
@@ -55,19 +130,6 @@ class NotificationController extends GetxController {
     print("_onMessage : $message");
     return null;
   }
-
   /* Google FCM Setting End */
 
-  /* OneSignal Setting Start */
-  // OneSignal 초기화
-  Future<void> _initOneSignal() async {
-    _oneSignal.init(oneSignalID, iOSSettings: {
-      OSiOSSettings.autoPrompt: false,
-      OSiOSSettings.inAppLaunchUrl: false,
-    });
-    _oneSignal.setInFocusDisplayType(OSNotificationDisplayType.notification);
-    await _oneSignal.promptUserForPushNotificationPermission(
-        fallbackToSettings: true);
-  }
-/* OneSignal SETTINGS END */
 }

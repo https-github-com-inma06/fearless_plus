1589a142a43a2cae7e690aeedd6eff1119d14d50
byungkyu.kim <kim.byungkyu@nhn-comico.com>
2021-06-03 23:44:10 +0900

    전체화면 모드 비활성화

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
 
     return GetMaterialApp(
       title: "Fearless_App",

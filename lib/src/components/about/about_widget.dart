8fa35582769e83a4cb3b45e9d07a490480694d10
byungkyu.kim <kim.byungkyu@nhn-comico.com>
2021-06-04 20:24:59 +0900

    about레이아웃 작업중

--- lib/src/components/about/about_widget.dart
+++ lib/src/components/about/about_widget.dart
@@ -11,16 +11,14 @@ class AboutWidget extends StatelessWidget {
       String insUrl, String twiUrl) {
     return SafeArea(
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(18),
+        borderRadius: BorderRadius.circular(12),
         child: Container(
-          width: 195,
-          height: 275,
           color: const Color(0xffFF9D6B),
           child: Column(
             children: [
               Container(
                 margin: const EdgeInsets.only(
-                  top: 25,
+                  top: 15,
                   bottom: 15,
                 ),
                 child: ClipRRect(
@@ -85,9 +83,98 @@ class AboutWidget extends StatelessWidget {
     );
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Container(
+            color: Colors.white,
+            child: CustomScrollView(
+              slivers: [
+                // SliverPersistentHeader(delegate: delegate),
+                SliverToBoxAdapter(),
+                SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        Container(
+                          child: Row(
+                            children: [
+                              Padding(padding: EdgeInsets.all(10),
+                                child: ClipRRect(
+                                  // borderRadius: BorderRadius.circular(0),
+                                  child: SvgPicture.asset(
+                                    "assets/svg/icons/ico_aboutbg_on.svg",
+                                    width: 50,
+                                    height: 50,
+                                  ),
+                                ),
+                              ),
+
+                              Column(
+
+                                children: [
+                                  Text(
+                                    "BRAVE GIRLS",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+
+                                  Row(
+                                    children: [
+                                      SvgPicture.asset(
+                                        'assets/svg/icons/ico_instagram.svg',
+                                        color: Colors.black,
+                                        width: 25,
+                                        height: 25,
+                                      ),
+                                      SizedBox(
+                                        width: 16,
+                                      ),
+                                      SvgPicture.asset(
+                                        'assets/svg/icons/ico_twitter.svg',
+                                        color: Colors.black,
+                                        width: 25,
+                                        height: 25,
+                                      ),
+                                    ],
+                                  )
+
+                                ],
+
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
+                SliverPadding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  sliver: SliverGrid(
+                      delegate: SliverChildListDelegate([
+                        _profile('assets/images/img_min.png1', 'MAINYOUNG',
+                            'Main Vocalist', '1990.09.12', '', ''),
+                        _profile('assets/images/img_yoo.png1', 'YOOJUNG',
+                            'Vocalist', '1991.05.02', '', ''),
+                        _profile(
+                            'assets/images/img_eun.png1', 'EUNJI', 'Vocalist',
+                            '1992.07.19', '', ''),
+                        _profile('assets/images/img_yuna.png', 'YUNA-😍',
+                            'Lead Vocalist', '1993.04.06', '', ''),
+                      ]),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          mainAxisSpacing: 0,
+                          crossAxisSpacing: 10,
+                          crossAxisCount: 2)),
+                )
+              ],
+            ),
+          )),
+
+      /*
       // margin: const EdgeInsets.only(top: 20, left: 20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -95,23 +182,24 @@ class AboutWidget extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _profile('assets/images/img_min.png', 'MAINYOUNG',
+              _profile('assets/images/img_min.png1', 'MAINYOUNG',
                   'Main Vocalist', '1990.09.12', '', ''),
-              _profile('assets/images/img_yoo.png', 'YOOJUNG', 'Vocalist',
+              _profile('assets/images/img_yoo.png1', 'YOOJUNG', 'Vocalist',
                   '1991.05.02', '', ''),
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _profile('assets/images/img_eun.png', 'EUNJI', 'Vocalist',
+              _profile('assets/images/img_eun.png1', 'EUNJI', 'Vocalist',
                   '1992.07.19', '', ''),
-              _profile('assets/images/img_yuna.png', 'YUNA-😍', 'Lead Vocalist',
+              _profile('assets/images/img_yuna.png1', 'YUNA-😍', 'Lead Vocalist',
                   '1993.04.06', '', ''),
             ],
           ),
         ],
       ),
+      */
     );
   }
 }

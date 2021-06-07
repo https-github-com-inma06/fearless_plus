1b26afd0326237df941423cb01354edd1f471036
byungkyu.kim <kim.byungkyu@nhn-comico.com>
2021-06-07 20:24:55 +0900

    About_BG layout

--- lib/src/components/about/about_widget.dart
+++ lib/src/components/about/about_widget.dart
@@ -124,6 +124,39 @@ class AboutWidget extends StatelessWidget {
     );
   }
 
+  Widget _community(String name, String url) {
+    return SafeArea(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          color: const Color(0xffFFEEE5),
+          child: Row(
+            children: [
+
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+
+              Text(
+                ">",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -143,7 +176,7 @@ class AboutWidget extends StatelessWidget {
                         child: ClipRRect(
                           // borderRadius: BorderRadius.circular(0),
                           child: SvgPicture.asset(
-                            "assets/svg/icons/ico_about_bg_logo.svg",
+                            "assets/svg/icons/ico_aboutbg_on.svg",
                             width: 50,
                             height: 50,
                           ),
@@ -153,7 +186,7 @@ class AboutWidget extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Padding(padding: EdgeInsets.fromLTRB(8, 0, 0, 10),
+                          Padding(padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
                             child: Text(
                               "BRAVE GIRLS",
                               style: TextStyle(
@@ -183,12 +216,54 @@ class AboutWidget extends StatelessWidget {
                       )
                     ],
                   ),
-                )
+                ),
+
+                GridView.count(crossAxisCount: 2,
+                children: [
+                  _profile(
+                      'assets/images/img_min.png',
+                      'MINYEONG🎤',
+                      'Main Vocalist',
+                      '1990.09.12',
+                      'nyong2ya',
+                      'nyong2ya',
+                      'UCM7sspcSzirLxsfIUt6i3Vw',
+                      "@bravegirls_my"),
+                  _profile(
+                      'assets/images/img_yoo.png',
+                      'YUJEONG🐢',
+                      'Vocalist',
+                      '1991.05.02',
+                      'braveg_yj',
+                      'bgyjnice',
+                      'UC0rYv-5_Ce72wegF9_pmDpw',
+                      "@yjistimeless"),
+                  _profile(
+                      'assets/images/img_eun.png',
+                      'EUNJI👀',
+                      'Vocalist',
+                      '1992.07.19',
+                      'bg_eunji92',
+                      'braveunji',
+                      '',
+                      "@bravegirls_eunji"),
+                  _profile(
+                      'assets/images/img_yuna.png',
+                      'YUNA👩',
+                      'Lead Vocalist',
+                      '1993.04.06',
+                      'u.nalee',
+                      'u_nalee_',
+                      'UCMLa547c-KJbOSzTf010Q1Q',
+                      "@bravegirls_u_na"),
+                ],)
+
+
               ],
             ),
           ),
           SliverPadding(
-            padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+            padding: EdgeInsets.fromLTRB(20, 5, 20, 25),
             sliver: SliverGrid(
                 delegate: SliverChildListDelegate([
                   _profile(
@@ -234,6 +309,34 @@ class AboutWidget extends StatelessWidget {
                     crossAxisSpacing: 20,
                     crossAxisCount: 2)),
           ),
+
+/*
+          SliverToBoxAdapter(
+            child: Container(
+              height: 20,
+              color: const Color(0xffF2F2F2),
+            ),
+          ),
+
+
+          SliverPadding(
+            padding: EdgeInsets.fromLTRB(20, 5, 20, 25),
+            sliver: SliverGrid(
+                delegate: SliverChildListDelegate([
+                  _community(
+                      '공식카페',
+                      "@bravegirls_eunji"),
+                  _community(
+                      '갤러리',
+                      "@bravegirls_u_na"),
+                ]),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 0.9,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20,
+                    crossAxisCount: 2)),
+          ),
+          */
         ],
       ),
     );

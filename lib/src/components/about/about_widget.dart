281a3150d7781c0f588f683b906501b252ddf634
inma06 <pakbongho@gmail.com>
2021-06-19 14:24:22 +0900

    [fix]  - 버저닝 1.0.3 / 13

--- lib/src/components/about/about_widget.dart
+++ lib/src/components/about/about_widget.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fearlessassemble/extension.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
